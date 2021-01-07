8f146a606f7ca6fd48018ea0c3bed0d1dd044598
DevMurloc <52956331+GiantMurloc@users.noreply.github.com>
2021-01-07 18:06:34 +0100

    0.1

---
+++ _site/collection.js
@@ -0,0 +1,21 @@
+var collection = JSON.parse(
+    {
+        "Hyouka": {
+            "isSeries": true,
+            "ep": {
+                "0": {},
+                "1": {
+                    "0": {
+                        "name": {
+                            "en": "The Esteemed Classic Literature Club Has Been Restored"
+                        },
+                        "stream": {
+                            "vivo": "cda905e64d"
+                        },
+                        "desc": {}
+                    }
+                }
+            }
+        }
+    }
+); 
